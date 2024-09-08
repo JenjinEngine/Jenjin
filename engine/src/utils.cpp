c5f073bb06c62ad031cbcca93b2feb2843bfd94d
Ben McAvoy <ben.mcavoy@tutanota.com>
2024-09-08 20:04:11 +0100

    fix: calculate ndc properly with camera

--- engine/src/utils.cpp
+++ engine/src/utils.cpp
@@ -2,9 +2,10 @@
 #include "utils.h"
 
 glm::vec2 screen_to_ndc(const glm::vec2& screen_pos) {
-    glm::vec2 inv_window_size = 2.0f / JenjinState.window_size;
-    glm::vec2 ndc_pos = (screen_pos * inv_window_size) - glm::vec2(1.0f);
-    ndc_pos = ndc_pos * JenjinState.camera->zoom + JenjinState.camera->position;
-    return ndc_pos;
+	glm::vec2 inv_window_size = 2.0f / JenjinState.window_size;
+	glm::vec2 ndc_pos = (screen_pos * inv_window_size) - glm::vec2(1.0f);
+	glm::vec2 camera_position = JenjinState.camera->position;
+	ndc_pos = ndc_pos * JenjinState.camera->zoom + camera_position;
+	return ndc_pos;
 }
 
